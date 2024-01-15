90c8d463f1e596296b6d7fc39615c7801e7d75a4
Monir Nasserie <58013715+MN-Coding@users.noreply.github.com>
2024-01-15 00:10:23 -0500

    updated api string

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-// const API_BASE_URL = 'https://gymbro-backend-wefl.onrender.com';
-const API_BASE_URL = 'http://localhost:4000';
+const API_BASE_URL = 'https://gymbro-backend-wefl.onrender.com';
+// const API_BASE_URL = 'http://localhost:4000';
 export default API_BASE_URL;
